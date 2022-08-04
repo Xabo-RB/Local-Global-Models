e9a210553e2c4c97944dcb69c32dbffe12d48f14
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-08-04 12:12:01 +0200

    New models from SI.jl

---
+++ QWWC_SI.jl
@@ -0,0 +1,17 @@
+using Logging
+
+using StructuralIdentifiability
+
+#QWWC
+logger = Logging.SimpleLogger(stdout, Logging.Debug)
+global_logger(logger)
+
+ode = @ODEmodel(
+    x'(t) = a * (y(t) - x(t)) + y(t) * z(t),
+    y'(t) = b * (x(t) + y(t)) - x(t) * z(t),
+    z'(t) = - c * z(t) - d * w(t) + x(t) * y(t),
+    w'(t) = e * z(t) - f * w(t) + x(t) * y(t),
+    g(t) = x(t)
+)
+
+println(assess_identifiability(ode))
