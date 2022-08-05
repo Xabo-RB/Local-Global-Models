fe6e972dd0b88ab697f935c5e27aa2bfcc522f56
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-08-05 12:22:59 +0200

    Create Biohydrogenation.jl

---
+++ SIANjl/Biohydrogenation.jl
@@ -0,0 +1,17 @@
+
+using SIAN, Logging
+
+@info "Setting up the problem"
+
+ode = @ODEmodel(
+  x4'(t) = -k5 * x4(t) // (k6 + x4(t)),
+  x5'(t) = k5 * x4(t) // (k6 + x4(t)) - k7 * x5(t) / (k8 + x5(t) + x6(t)),
+  x6'(t) = k7 * x5(t) // (k8 + x5(t) + x6(t)) - k9 * x6(t) * (k10 - x6(t)) // k10,
+  x7'(t) = k9 * x6(t) * (k10 - x6(t)) // k10,
+  y1(t) = x4(t),
+  y2(t) = x5(t)
+)
+
+res = identifiability_ode(ode, get_parameters(ode); p = 0.999, p_mod = 0, nthrds = 1)
+
+println(res)
