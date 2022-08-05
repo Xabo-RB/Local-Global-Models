ece50dcdf6eae04a698b0de5e0d2bb9a4ac98735
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-08-05 13:26:35 +0200

    New models

---
+++ SIANjl/oralGlucose.jl
@@ -0,0 +1,19 @@
+using SIAN, Logging
+@info "Setting up the problem"
+
+ode = @ODEmodel(
+  G'(t) = -(p1 + X(t)) * G(t) + p1 * Gb(t) + v * R(t),
+  X'(t) = -p2 * X(t) + p3 * (u(t) - Ib(t)),
+  R'(t) = k,
+  Ib'(t) = 0,
+  Gb'(t) = 0,
+  y1(t) = G(t),
+  y2(t) = Ib(t),
+  y3(t) = Gb(t)
+)
+
+res = identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 0, nthrds = 1)
+
+println(res)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
