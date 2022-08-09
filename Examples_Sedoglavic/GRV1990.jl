322c5fbd4c3251388d145710e3696b88575ee392
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-08-09 12:52:08 +0200

    New

--- Examples_Sedoglavic/GRV1990.jl
+++ Examples_Sedoglavic/GRV1990.jl
@@ -1,8 +1,8 @@
 using StructuralIdentifiability
 
 ode = @ODEmodel(
-    x1'(t) = x1(t)*(p1*x2(t)/(p2+x2(t)) - p3) + p4*u(t),
-    x2'(t) = - p1*x1(t)*x2(t)/(p5*(p2+x2(t))) + p6*u(t),
+    x1'(t) = p1*x1(t)^2 + p2*x1(t)*x2(t) + u(t),
+    x2'(t) = p3*x1(t)^2 + p4*x1(t)*x2(t),
     y1(t) = x1(t)
 )
 
