1fa58ade92f8c2d06684b88dc51e2c06ed9416b9
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-08-10 13:41:22 +0200

    Update MP1999.jl

--- Examples_Sedoglavic/StructuralIdentifiability/MP1999.jl
+++ Examples_Sedoglavic/StructuralIdentifiability/MP1999.jl
@@ -4,7 +4,7 @@ ode = @ODEmodel(
     x1'(t) = x3(t) - x2(t)*u(t),
     x2'(t) = u(t) - x2(t),
     x3'(t) = x2(t) - x1(t) + 2*x2(t)*(u(t)-x2(t)),
-    y1(t) = x1(t)+x2(t)^2/2
+    y1(t) = x1(t)+(x2(t)^2)/2
 )
 
 @time println(assess_identifiability(ode))
