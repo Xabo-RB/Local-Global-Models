4021cda99ec938c308ae922a92dfa1943900e8b3
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-08-13 18:37:59 +0200

    input, known constants...

--- Examples_SIANjl/DAISY_ex3.jl
+++ Examples_SIANjl/DAISY_ex3.jl
@@ -46,3 +46,35 @@ ode = @ODEmodel(
 res = identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 0, nthrds = 1)
 
 println(res)
+
+#MODELO CON MENOS PARAMETROS CON U0 = CONSTANTE -> MISMO RESULTADOS
+using SIAN, Logging
+
+ode = @ODEmodel(
+  x1'(t) = -1 * p1 * x1(t) + x2(t) + u0(t),
+  x2'(t) = p3 * x1(t) - p4 * x2(t) + x3(t),
+  x3'(t) = p6 * x1(t) - p7 * x3(t),
+  u0'(t) = 1,
+  y(t) = x1(t),
+  y2(t) = u0(t)
+)
+
+res = identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 0, nthrds = 1)
+
+println(res)
+
+#MODELO CON MENOS PARAMETROS CON U0 = CONSTANTE -> MISMO RESULTADOS
+using SIAN, Logging
+
+ode = @ODEmodel(
+  x1'(t) = -1 * p1 * x1(t) + x2(t) + u0(t),
+  x2'(t) = p3 * x1(t) - p4 * x2(t) + x3(t),
+  x3'(t) = p6 * x1(t) - p7 * x3(t),
+  u0'(t) = 0,
+  y(t) = x1(t),
+  y2(t) = u0(t)
+)
+
+res = identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 0, nthrds = 1)
+
+println(res)
--- Examples_SIANjl/Treatment.jl
+++ Examples_SIANjl/Treatment.jl
@@ -14,3 +14,15 @@ res = identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 0, nthrds 
 println(res)
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+#N como una entrada u(t)
+using SIAN, Logging
+
+ode = @ODEmodel(
+  S'(t) = -b * S(t) * In(t) / u(t) - d * b * S(t) * Tr(t) / u(t),
+  In'(t) = b * S(t) * In(t) / u(t) + d * b * S(t) * Tr(t) / u(t) - (a + g) * In(t),
+  Tr'(t) = g * In(t) - nu * Tr(t),
+  y1(t) = Tr(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
