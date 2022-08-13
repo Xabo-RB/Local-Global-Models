a0ca2c2b13a50f238888b67082e89d6f7471a41e
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-08-13 14:21:50 +0200

    Update DAISY_mamil3.jl

--- Examples_SIANjl/DAISY_mamil3.jl
+++ Examples_SIANjl/DAISY_mamil3.jl
@@ -13,3 +13,18 @@ res = identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 0, nthrds 
 println(res)
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+#SIN VARIABLE DE ENTRADA
+
+using SIAN, Logging
+
+ode = @ODEmodel(
+  x1'(t) = -(a21 + a31 + a01) * x1(t) + a12 * x2(t) + a13 * x3(t),
+  x2'(t) = a21 * x1(t) - a12 * x2(t),
+  x3'(t) = a31 * x1(t) - a13 * x3(t),
+  y(t) = x1(t)
+)
+
+res = identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 0, nthrds = 1)
+
+println(res)
