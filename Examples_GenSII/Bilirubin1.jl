1a163d6055c8a61e50cf9e513cf7709aae7acb92
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-08-14 19:07:09 +0200

    Update Bilirubin1.jl

--- Examples_GenSII/Bilirubin1.jl
+++ Examples_GenSII/Bilirubin1.jl
@@ -1,7 +1,7 @@
 using StructuralIdentifiability
 
 ode = @ODEmodel(
-    x1'(t) = -k31*x1(t)+k13*x3(t),
+    x1'(t) = -k31*x1(t)+k13*x3(t) + u(t),
     x2'(t) = -k42*x2(t)+k24*x4(t),
     x3'(t) = k31*x1(t)-(k03+k13+k43)*x3(t),
     x4'(t) = k42*x2(t)+k43*x3(t)-(k04+k24)*x4(t),
