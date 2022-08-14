42b9007dd7e84e8703943ae6153fdb644fd28dea
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-08-14 14:56:53 +0200

    Create Transfection_2State.jl

---
+++ Examples_GenSII/Transfection_2State.jl
@@ -0,0 +1,9 @@
+using SIAN, Logging
+
+ode = @ODEmodel(
+  mRNA'(t) = -d*mRNA,
+  GFP'(t) =   kTL*mRNA*u1(t) - b*GFP*u2(t),
+  y1(t) = GFP(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
