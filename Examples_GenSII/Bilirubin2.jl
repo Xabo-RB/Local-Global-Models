d3fb0883cd9e0b206cfc82265da259295c7f71d4
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-08-14 11:53:05 +0200

    Delete

--- Examples_GenSII/Bilirubin2.jl
+++ Examples_GenSII/Bilirubin2.jl
@@ -1,10 +1,10 @@
 using SIAN, Logging
 
 ode = @ODEmodel(
-  x1'(t) = -(k21+k31+k41+k01)*x1+k12*x2+k13*x3+k14*x4,
-  x2'(t) = k21*x1-k12*x2,
-  x3'(t) = k31*x1-k13*x3,
-  x4'(t) = k41*x1-k14*x4,
+  x1'(t) = -(k21+k31+k41+k01)*x1(t)+k12*x2(t)+k13*x3(t)+k14*x4(t),
+  x2'(t) = k21*x1(t)-k12*x2(t),
+  x3'(t) = k31*x1(t)-k13*x3(t),
+  x4'(t) = k41*x1(t)-k14*x4(t),
   y1(t) = x1(t)
 )
 
