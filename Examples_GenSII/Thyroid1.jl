ef2e44c3ba21c59f056dbf4647c2ea4473360714
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-08-14 14:58:33 +0200

    Update Thyroid1.jl

--- Examples_GenSII/Thyroid1.jl
+++ Examples_GenSII/Thyroid1.jl
@@ -8,3 +8,15 @@ ode = @ODEmodel(
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+
+using StructuralIdentifiability
+
+ode = @ODEmodel(
+    x1'(t) = k13*x3(t)+k12*x2(t)-(k21+k31)*x1(t) + u(t),
+    x2'(t) =  k21*x1(t)-(k12+k02)*x2(t),
+    x3'(t) = k31*x1(t)-(k13+k03)*x3(t),
+    y1(t) = x1(t)/V1
+)
+
+@time println(assess_identifiability(ode))
