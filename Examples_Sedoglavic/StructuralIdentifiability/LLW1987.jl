755f4bec24e1369aa62217ef6088cd15e71eb49e
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-08-16 11:25:04 +0200

    Update LLW1987.jl

--- Examples_Sedoglavic/StructuralIdentifiability/LLW1987.jl
+++ Examples_Sedoglavic/StructuralIdentifiability/LLW1987.jl
@@ -8,3 +8,17 @@ ode = @ODEmodel(
 )
 
 @time println(assess_identifiability(ode))
+
+
+#SIN u(t)
+using StructuralIdentifiability
+
+ode = @ODEmodel(
+    x1'(t) = - p1*x1(t) + p2,
+    x2'(t) = - p3*x2(t) + p4,
+    x3'(t) = - (p1+p3)*x3(t) + (p4*x1(t)+p2*x2(t)),
+    y1(t) = x3(t)
+)
+
+@time println(assess_identifiability(ode))
+
