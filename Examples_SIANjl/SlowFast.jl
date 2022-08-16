bbcb4355d93e311b617d0b34318cdda11741be28
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-08-16 18:21:15 +0200

    Update SlowFast.jl

--- Examples_SIANjl/SlowFast.jl
+++ Examples_SIANjl/SlowFast.jl
@@ -18,12 +18,21 @@ println(res)
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
 
-# The following computation shows that one can identify more from two experiments
-# SIAN. generate_replica(ode, 2) generates a system consiting of two copies of sigma
-# with the same parameters in ODEs but different inputs and initial conditions
 
-r = 2
+#SIN U O CONSTANTES CONOCIDAS
 
-res = identifiability_ode(SIAN.generate_replica(ode, r), get_parameters(ode; initial_conditions = false); p = 0.99, p_mod = 0, nthrds = 1)
+using SIAN, Logging
+
+ode = @ODEmodel(
+  xA'(t) = -k1 * xA(t),
+  xB'(t) = k1 * xA(t) - k2 * xB(t),
+  xC'(t) = k2 * xB(t),
+  y1(t) = xC(t),
+  y2(t) = xA(t) + xB(t) + xC(t)
+)
+
+res = identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 0, nthrds = 1)
 
 println(res)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
