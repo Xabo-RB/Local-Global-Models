a766ae72a0bf1f5fa28ae8b702343d13ad1892c4
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-08-29 11:58:52 +0200

    Update CSTR_observer.jl

--- Examples_StrikeGoldd/SIAN SI jl/CSTR_observer.jl
+++ Examples_StrikeGoldd/SIAN SI jl/CSTR_observer.jl
@@ -3,6 +3,7 @@ ode = @ODEmodel(
     Cr'(t) = q*(u1(t)-Cr(t))-0.072*(exp(gamma*Tr(t)/(gamma+Tr(t)/20)))*Cr(t)+d,
     Tr'(t) =  q*(u2(t)-Tr(t))+0.576*(exp(gamma*Tr(t)/(gamma+Tr(t)/20)))*Cr(t)-0.3*(Tr(t)-Tc(t)),
     Tc'(t) = delta1*qc*(u3(t)-Tc(t))+3.0*(Tr(t)-Tc(t)),
+    
     y1(t) = Tc(t)
 )
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
