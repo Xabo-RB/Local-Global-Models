6edeca1e0cbf85facb9a0ea28a7de66c8ab09d27
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-08-31 10:22:25 +0200

    Update SEUIR.jl

--- SEUIR.jl
+++ SEUIR.jl
@@ -5,8 +5,20 @@ ode = @ODEmodel(
     S'(t) = -beta* (U(t)+I(t)) *(S(t)/N),
     E'(t) =  beta*(U(t)+I(t))*(S(t)/N) - E(t)/Z,
     U'(t) = (1-alpha)*E(t)/Z - U(t)/D,
-    U'(t) = alpha*E(t)/Z - I(t)/D,
+    I'(t) = alpha*E(t)/Z - I(t)/D,
     R'(t) = U(t)/D + I(t)/D,
+    y1(t) = I(t)
+)
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+
+using SIAN, Logging
+ode = @ODEmodel(
+    S'(t) = -beta* (U(t)+I(t)) *(S(t)/N),
+    E'(t) =  beta*(U(t)+I(t))*(S(t)/N) - E(t)*z,
+    U'(t) = (z-w)*E(t) - U(t)*d,
+    I'(t) = w*E(t) - I(t)*d,
+    R'(t) = (U(t) + I(t))*d,
     y1(t) = I(t)
 )
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
