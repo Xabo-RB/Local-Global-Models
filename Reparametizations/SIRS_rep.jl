c3d0b13af5e8dfa9d2a71917f23ba40ba5aeeae1
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-09-06 11:42:23 +0200

    Update SIRS_rep.jl

--- Reparametizations/SIRS_rep.jl
+++ Reparametizations/SIRS_rep.jl
@@ -3,11 +3,11 @@ using Logging, SIAN
 
 
 ode = @ODEmodel(
-    s'(t) = mu + g*r(t) - mu*s(t) - b0*i(t)*s(t) - b0*i(t)*s(t)*x1(t),
-    x2'(t) = M*x1(t),
-    i'(t) = -i(t)*(mu + nu - b0*s(t) - b0*s(t)*x1(t)),
-    r'(t) = i(t)*nu - g*r(t) - mu*r(t),
-    x1'(t) = -M*x2(t),
+    s'(t) = mu - mu * s(t) - b0 * (1 + 1 * x1(t)) * i(t) * s(t) + g * r(t),
+    i'(t) = b0 * (1 + 1 * x1(t)) * i(t) * s(t) - (nu + mu) * i(t),
+    r'(t) = nu * i(t) - (mu + g) * r(t),
+    x1'(t) = -M * x2(t),
+    x2'(t) = M * x1(t),
     y1(t) = i(t),
     y2(t) = r(t)
 )
