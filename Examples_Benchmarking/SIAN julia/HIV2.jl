0661cf1e4b81aeb7dc8b8f7fecdc6973ef3cfbff
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-09-13 13:03:27 +0200

    Update HIV2.jl

--- Examples_Benchmarking/SIAN julia/HIV2.jl
+++ Examples_Benchmarking/SIAN julia/HIV2.jl
@@ -14,4 +14,26 @@ ode = @ODEmodel(
     y2(t) = x4(t)
 )
 
-@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 0, nthrds = 1))
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+
+
+#New outputs
+
+using Logging, SIAN
+
+
+ode = @ODEmodel(
+    x1'(t) = -b*x1(t)*x4(t) - d*x1(t) + s,
+    x2'(t) = b*q1(t)*x1(t)*x4(t)-k1*x2(t)-w1*x2(t),
+    x3'(t) = b*q2*x1(t)*x4(t)+k1*x2(t)-w2*x3(t),
+    x4'(t) = -c*x4(t)+k2(t)*x3(t),
+    q1'(t) = 0,
+    k2'(t) = 0,
+    y1(t) = x1(t),
+    y2(t) = x4(t),
+    y3(t) = k2(t),
+    y4(t) = q1(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
