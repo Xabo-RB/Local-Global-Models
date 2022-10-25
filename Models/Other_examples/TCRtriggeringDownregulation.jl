38a8fa0959a5f23fa72b26aef4aebd4285c3f3ca
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-10-25 12:16:46 +0200

    Update TCRtriggeringDownregulation.jl

--- Models/Other_examples/TCRtriggeringDownregulation.jl
+++ Models/Other_examples/TCRtriggeringDownregulation.jl
@@ -6,8 +6,8 @@ using SIAN, Logging
 
 ode = @ODEmodel(
     S'(t) = -lambda * phi * (S(t)-T(t)) + s*(1-S(t)),
-    T'(t) =  phi * (S(t) - T(t)) + s*(1-T(t)) - k * T(t)*L,
-    A'(t) =  k * T(t) - L - ki*A(t),
+    T'(t) =  phi * (S(t) - T(t)) + s*(1-T(t)) - k * (T(t)^5)*(L^5),
+    A'(t) =  k * (T(t)^5)*(L^5) - ki*A(t),
     y1(t) = S(t)/(lambda+1) + ((T(t)+A(t))*lambda/(lambda+1))
 )
 
