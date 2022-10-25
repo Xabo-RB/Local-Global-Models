fd507a4239992b4280c91a18a51dcbb6306e25aa
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-10-25 11:43:07 +0200

    Update TCRtriggeringDownregulationPotenciaH.m

--- Models/Other_examples/TCRtriggeringDownregulationPotenciaH.m
+++ Models/Other_examples/TCRtriggeringDownregulationPotenciaH.m
@@ -1,32 +1,62 @@
+%% MODELO E (h = cte)
+
 syms S T A
 x = [S; T; A];
-
-% 1 output
-h = S/(lambda+1) + ((T+A)*lambda/(lambda+1));
 
 % one input
 syms u1;
 u = [];
 
 % 4 unknown parameters 
-syms lambda phi s ki h
-p =[lambda; phi; s; ki; h];
+syms lambda phi s ki hh k L
+p =[lambda; phi; s; ki; hh; k; L];
 
 % 1 output
 h = S/(lambda+1) + ((T+A)*lambda/(lambda+1));
 
 % dynamic equations
 f = [-lambda * phi * (S-T) + s*(1-S);
-    phi * (S - T) + s*(1-T) - k * (T^h)*(L^h);
-    k *(T^h)*(L^h) - ki*A];
+    phi * (S - T) + s*(1-T) - k * (T^hh)*(L^hh);
+    k *(T^hh)*(L^hh) - ki*A];
+
+% 1 outpu
+h = S/(lambda+1) + ((T+A)*lambda/(lambda+1));
 
 % initial conditions
 ics  = []; 
-known_ics = [0,0];
+known_ics = [0,0,0];
 
-save('C2M','x','p','h','f','u','ics','known_ics');
+save('TCRtrig','x','p','h','f','u','ics','known_ics');
 
 
+
+%% MODELO E (h unknown)
+
+syms S T A
+x = [S; T; A];
+
+% one input
+syms u1;
+u = [];
+
+% 4 unknown parameters 
+syms lambda phi s ki k L
+p =[lambda; phi; s; ki; k; L];
+
+
+% dynamic equations
+f = [-lambda * phi * (S-T) + s*(1-S);
+    phi * (S - T) + s*(1-T) - k * (T^5)*(L^5);
+    k *(T^5)*(L^5) - ki*A];
+
+% 1 output
+h = S/(lambda+1) + ((T+A)*lambda/(lambda+1));
+
+% initial conditions
+ics  = []; 
+known_ics = [0,0,0];
+
+save('TCRtrig','x','p','h','f','u','ics','known_ics');
 
 
 
