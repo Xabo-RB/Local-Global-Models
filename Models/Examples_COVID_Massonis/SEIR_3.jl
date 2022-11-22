26f384e50cb9e0a2f46bef93d33d4262b158a7ef
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-11-22 15:24:32 +0100

    Create SEIR_3.jl

---
+++ Models/Examples_COVID_Massonis/SEIR_3.jl
@@ -0,0 +1,18 @@
+# Liangrong Peng et al. (2020) Epidemic analysis of COVID-19 in 
+# China by dynamical modeling 
+
+
+using SIAN, Logging
+ode = @ODEmodel(
+    S'(t) = beta*I(t)*S(t)/N-alpha*S(t),
+    E'(t) =  beta*I(t)*S(t)/N-gamma*E(t),
+    I'(t) = gamma*E(t)-delta*I(t),
+    R'(t) = delta*I(t)-alpha*Q(t)-k*Q(t),
+    Q'(t) = alpha*Q(t),
+    D'(t) = k*Q(t),
+    P'(t) = alpha*S(t),
+    y1(t) = Q(t),
+    y2(t) = D(t),
+    y3(t) = R(t)
+)
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
