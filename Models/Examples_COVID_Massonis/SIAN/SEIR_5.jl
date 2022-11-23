a536b3b140bea4ce80e334251e2a943a0cd1bb3c
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-11-23 11:15:39 +0100

    Update SEIR_5.jl

--- Models/Examples_COVID_Massonis/SIAN/SEIR_5.jl
+++ Models/Examples_COVID_Massonis/SIAN/SEIR_5.jl
@@ -1,13 +1,15 @@
 # Mathematical Modeling of Epidemic Diseases; A Case Study of the COVID-19 Coronavirus
 
 using SIAN, Logging
+
 ode = @ODEmodel(
-    S'(t) = -alpha_e*S(t)*E(t)-alpha_i*S(t)*I(t),
-    E'(t) =  alpha_e*S(t)*E(t)+alpha_i*S(t)*I(t)-k*E(t)-rho*E(t),
-    I'(t) = k*E(t)-beta*I(t)-mu*I(t),
-    R'(t) = beta*I(t)+rho*E(t),
-    P'(t) = mu*I(t),
-    y1(t) = P(t)
+    S'(t) = -beta*(I(t)+A(t))*S(t),
+    E'(t) = beta*(I(t)+A(t)*S(t)-gamma*E(t)),
+    I'(t) = gamma*pp*E(t)-mu1*I(t),
+    A'(t) = gamma*(1-pp)*E(t)-mu2*A(t),
+    R'(t) = mu1*I(t)+mu2*A(t),
+    y1(t) = I(t)
 )
+
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
 
