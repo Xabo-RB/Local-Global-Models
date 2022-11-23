9ef29df380f4c77b3885f38f96d12391f333eea2
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-11-23 12:09:38 +0100

    N

--- Models/Examples_COVID_Massonis/SIAN/SEIR_2.jl
+++ Models/Examples_COVID_Massonis/SIAN/SEIR_2.jl
@@ -5,8 +5,9 @@
 
 using SIAN, Logging
 ode = @ODEmodel(
-    S'(t) = -beta*I(t)*S(t)/N,
-    E'(t) =  beta*I(t)*S(t)/N-k*E(t),
+    N'(t) = 0,
+    S'(t) = -beta*I(t)*S(t)/N(t),
+    E'(t) =  beta*I(t)*S(t)/N(t)-k*E(t),
     I'(t) = k*E(t)-gamma*I(t),
     R'(t) = gamma*I(t),
     C'(t) = k*E(t),
--- Models/Examples_COVID_Massonis/SIAN/SEIR_3.jl
+++ Models/Examples_COVID_Massonis/SIAN/SEIR_3.jl
@@ -4,8 +4,9 @@
 
 using SIAN, Logging
 ode = @ODEmodel(
-    S'(t) = beta*I(t)*S(t)/N-alpha*S(t),
-    E'(t) =  beta*I(t)*S(t)/N-gamma*E(t),
+    N'(t) = 0,
+    S'(t) = beta*I(t)*S(t)/N(t)-alpha*S(t),
+    E'(t) =  beta*I(t)*S(t)/N(t)-gamma*E(t),
     I'(t) = gamma*E(t)-delta*I(t),
     R'(t) = delta*I(t)-alpha*Q(t)-k*Q(t),
     Q'(t) = alpha*Q(t),
