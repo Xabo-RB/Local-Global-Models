4c89de02d1ac6b08cc0a834ee552394fa877907a
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-11-23 11:33:30 +0100

    8

--- Models/Examples_COVID_Massonis/SIAN/SEIR_8.jl
+++ Models/Examples_COVID_Massonis/SIAN/SEIR_8.jl
@@ -7,9 +7,13 @@ ode = @ODEmodel(
     S'(t) = -beta*(I(t)+eta*A(t))*S(t)/N,
     E'(t) = beta*(I(t)+eta*A(t))*S(t)/N-sigma*E(t),
     I'(t) = alpha*sigma*E(t)-phi*I(t)-gamma_i*I(t),
-    A'(t) = gamma*(1-pp)*E(t)-mu2*A(t),
-    R'(t) = mu1*I(t)+mu2*A(t),
-    y1(t) = I(t)
+    A'(t) = (1-alpha)*sigma*E(t)-gamma_0*A(t),
+    H'(t) = phi*I(t)-delta*H(t)-gamma_h*H(t),
+    R'(t) = gamma_i*I(t)+gamma_0*A(t)+gamma_h*H(t),
+    D'(t) = delta*H(t),
+    y1(t) = I(t),
+    y2(t) = H(t),
+    y3(t) = D(t)
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
