af02a3595c5a17552ac6ef77a7ca5bec70ee1ee1
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-11-30 13:21:43 +0100

    Update SEIR_36_ref.jl

--- Models/Examples_COVID_Massonis/SIAN/SEIR_36_ref.jl
+++ Models/Examples_COVID_Massonis/SIAN/SEIR_36_ref.jl
@@ -4,8 +4,9 @@ using SIAN, Logging
 ode = @ODEmodel(
     N'(t) = 0,
     nu'(t) = 0,
-    S'(t) = -beta*S(t)*I(t)/N(t)-q*beta_d*S(t)*Di(t)/N(t)+nu(t)*N(t)-mu_0*S(t),
-    E'(t) = beta*S(t)*I(t)/N(t)+q*beta_d*S(t)*Di(t)/N(t)-s*E(t)-phi_e*E(t)-mu_0*E(t),
+    q'(t) = 0,
+    S'(t) = -beta*S(t)*I(t)/N(t)-q(t)*beta_d*S(t)*Di(t)/N(t)+nu(t)*N(t)-mu_0*S(t),
+    E'(t) = beta*S(t)*I(t)/N(t)+q(t)*beta_d*S(t)*Di(t)/N(t)-s*E(t)-phi_e*E(t)-mu_0*E(t),
     I'(t) = s*E(t)-gamma*I(t)-mu_i*I(t)-phi*I(t)-mu_0*I(t),
     De'(t) = phi_e*E(t)-s_d*De(t)-mu_0*De(t),
     Di'(t) = phi*I(t)+s_d*De(t)-gamma_d*Di(t)-mu_d*Di(t)-mu_0*Di(t),
@@ -15,7 +16,8 @@ ode = @ODEmodel(
     y2(t) = Di(t),
     y5(t) = F(t),
     y3(t) = N(t),
-    y4(t) = nu(t)
+    y4(t) = nu(t),
+    y5(t) = q(t)
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
