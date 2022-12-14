8d792ef77a183b50ddbd8e0ab8335f623e1d94f8
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-12-14 12:41:23 +0100

    Update generalizedLoktaVolterra.jl

--- Models/Other_examples/generalizedLoktaVolterra.jl
+++ Models/Other_examples/generalizedLoktaVolterra.jl
@@ -10,3 +10,15 @@ ode = @ODEmodel(
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+#Output 2
+
+using SIAN, Logging
+ode = @ODEmodel(
+    x1'(t) = r1*x1(t)+beta11*x1(t)^2+beta12*x1(t)*x2(t),
+    x2'(t) = r2*x2(t)+beta21*x1(t)*x2(t)+beta22*x2(t)^2,
+    y1(t) = x1(t)/(x1(t)+x2(t)),
+    y2(t) = x2(t)/(x1(t)+x2(t))
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
