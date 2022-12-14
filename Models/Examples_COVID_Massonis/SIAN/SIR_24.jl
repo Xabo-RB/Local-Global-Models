450f3e700590e5432bf135be42d5a4f99cbce95e
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-12-14 12:02:07 +0100

    Update SIR_24.jl

--- Models/Examples_COVID_Massonis/SIAN/SIR_24.jl
+++ Models/Examples_COVID_Massonis/SIAN/SIR_24.jl
@@ -8,7 +8,7 @@ ode = @ODEmodel(
     S'(t) = A(t)-mu+S(t)-c*phi*I(t)*S(t)/(I(t)+S(t)),
     I'(t) = -mu*I(t)+c*phi*I(t)*S(t)/(I(t)+S(t))-gamma*I(t)-I(t)*u1(t)/(S(t)+I(t)),
     R'(t) = -mu*R(t)+gamma*I(t)+I(t)*u1(t)/(S(t)+I(t)),
-    y1(t) = K(t)*I(t)
+    y1(t) = K*I(t)
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
