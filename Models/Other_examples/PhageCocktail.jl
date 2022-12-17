078756a877cba680e83d95f3a63d5c11ffa0b80e
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-12-17 19:50:04 +0100

    Update PhageCocktail.jl

--- Models/Other_examples/PhageCocktail.jl
+++ Models/Other_examples/PhageCocktail.jl
@@ -45,3 +45,21 @@ ode = @ODEmodel(
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+
+using SIAN, Logging
+ode = @ODEmodel(
+    x1'(t) = r1*x1(t)*(1-((x1(t)+x2(t))/kCD))*(1-m)-kPD*x1(t)*Y*x3(t)/(1+x3(t))-((E1*x4(t)*x1(t))/(1+x1(t)+x2(t))),
+    x2'(t) = r2*x2(t)*(1-((x1(t)+x2(t))/kCD))+r1*x1(t)*(1-((x1(t)+x2(t))/kCD))*m-kPD*x2(t)*Y*x5(t)/(1+x5(t))-((E1*x4(t)*x2(t))/(1+x1(t)+x2(t))),
+    x3'(t) = beta*x1(t)*Y*x3(t)/(1+x3(t))-Y*x1(t)*x3(t)-v*x3(t)+q*u1(t),
+    x4'(t) = alpha*x4(t)*(1-x4(t))*((x1(t)+x2(t))/(x1(t)+x2(t)+kND)),
+    x5'(t) = beta*x2(t)*Y*x5(t)/(1+x5(t))-Y*x2(t)*x5(t)-v*x5(t)+q*u2(t),
+    y1(t) = x4(t),
+    y2(t) = x2(t),
+    y3(t) = x1(t),
+    y4(t) = x3(t)
+
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
