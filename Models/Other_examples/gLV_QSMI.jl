cf918a0e5c38ad9a9cedfc12641d4e57ea30eda8
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-12-19 13:15:22 +0100

    Update gLV_QSMI.jl

--- Models/Other_examples/gLV_QSMI.jl
+++ Models/Other_examples/gLV_QSMI.jl
@@ -4,8 +4,12 @@
 
 using SIAN, Logging
 ode = @ODEmodel(
-    x1'(t) = r1*x1(t)+beta11*x1(t)^2+beta12*x1(t)*x2(t),
-    x2'(t) = r2*x2(t)+beta21*x1(t)*x2(t)+beta22*x2(t)^2,
+    x1'(t) = k11*x1(t)*w1(t) - d1*x1(t) + phi12*x1(t)*w2(t),
+    x2'(t) = k21*x2(t)*w1(t) - d2*x(2),
+    w1'(t) = f1 - dd1*w1(t) - k11*x1(t)*w1(t) - k21*x2(t)*w1(t),
+    w2'(t) = k21*x2(t)*w1(t) - dd2*w2(t) - k12*x1(t)*w2(t),
+    #yy1'(t) = k11*x1(t)*y1(t) - ddd1*yy1(t) - kk21*x2(t)*yy1(t),
+    #yy2'(t) = kk21*x2(t)*yy1(t) - ddd2*yy2(t) - kk32*x3(t)*yy2(t),
     y1(t) = x1(t),
     y2(t) = x2(t)
 )
