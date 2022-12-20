254b880b00ea7029c2c04bfc5e8409ea619ba90d
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-12-20 11:55:53 +0100

    Update TumorPillis2007.jl

--- Models/Examples_Inmunology/SIAN/TumorPillis2007.jl
+++ Models/Examples_Inmunology/SIAN/TumorPillis2007.jl
@@ -9,8 +9,8 @@ ode = @ODEmodel(
     L'(t) = -m*L(t) - q*L(t)*T(t)-ucte*L(t)^2+r2*C(t)*T(t) + pI*L(t)*I(t)/(gI+I(t)) + u1(t) - KL*M(t)*L(t), # tumor-specific effector cells, T-celss
     N'(t) = alpha1 - f*N(t)+ g*T(t)*N(t)/(h + T(t)) - p*N(t)*T(t) - KN*M(t)*N(t), # non-specific effector cells, NK cells
     C'(t) = alpha2 - beta*C(t) - KC*M(t)*C(t), #circulating lymphocytes
-    I'(t) = pt*T(t)*L(t)/(gt + T(t)) +  w*L(t)*I(t) - muI*I(t) + VI(t), # IL-2
-    M'(t) = - gamma*M(t) + VM(t), #chemotherapy drug
+    I'(t) = pt*T(t)*L(t)/(gt + T(t)) +  w*L(t)*I(t) - muI*I(t) + u2(t), # IL-2, VI = u2 aplicación directa, terapia de IL2
+    M'(t) = - gamma*M(t) + u1(t), #chemotherapy drug, terapia/aplicación de quimio, u1 = VM
     y1(t) = L(t),
     y2(t) = N(t),
     y3(t) = M(t)
@@ -28,8 +28,8 @@ ode = @ODEmodel(
     L'(t) = -m*L(t) - q*L(t)*T(t)-ucte*L(t)^2+r2*C(t)*T(t) + pI*L(t)*I(t)/(gI+I(t)) + u1(t) - KL*M(t)*L(t), # tumor-specific effector cells, T-celss
     N'(t) = alpha1 - f*N(t)+ g*T(t)*N(t)/(h + T(t)) - p*N(t)*T(t) - KN*M(t)*N(t), # non-specific effector cells, NK cells
     C'(t) = alpha2 - beta*C(t) - KC*M(t)*C(t), #circulating lymphocytes
-    I'(t) = pt*T(t)*L(t)/(gt + T(t)) +  w*L(t)*I(t) - muI*I(t) + VI(t), # IL-2
-    M'(t) = - gamma*M(t) + VM(t), #chemotherapy drug
+    I'(t) = pt*T(t)*L(t)/(gt + T(t)) +  w*L(t)*I(t) - muI*I(t) + u2(t), # IL-2, VI = u2 aplicación directa, terapia de IL2
+    M'(t) = - gamma*M(t) + u1(t), #chemotherapy drug, terapia/aplicación de quimio, u1 = VM
     y1(t) = L(t) + N(t) + C(t),
     y2(t) = M(t)
 )
@@ -45,14 +45,15 @@ ode = @ODEmodel(
     L'(t) = -m*L(t) - q*L(t)*T(t)-ucte*L(t)^2+r2*C(t)*T(t) + pI*L(t)*I(t)/(gI+I(t)) + u1(t) - KL*M(t)*L(t), # tumor-specific effector cells, T-celss
     N'(t) = alpha1 - f*N(t)+ g*T(t)*N(t)/(h + T(t)) - p*N(t)*T(t) - KN*M(t)*N(t), # non-specific effector cells, NK cells
     C'(t) = alpha2 - beta*C(t) - KC*M(t)*C(t), #circulating lymphocytes
-    I'(t) = pt*T(t)*L(t)/(gt + T(t)) +  w*L(t)*I(t) - muI*I(t) + VI(t), # IL-2
-    M'(t) = - gamma*M(t) + VM(t), #chemotherapy drug
+    I'(t) = pt*T(t)*L(t)/(gt + T(t)) +  w*L(t)*I(t) - muI*I(t) + u2(t), # IL-2, VI = u2 aplicación directa, terapia de IL2
+    M'(t) = - gamma*M(t) + u1(t), #chemotherapy drug, terapia/aplicación de quimio, u1 = VM
     y1(t) = L(t) + N(t) + C(t),
     y2(t) = M(t),
     y3(t) = I(t)
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
 
 #
 using SIAN, Logging
@@ -62,8 +63,8 @@ ode = @ODEmodel(
     L'(t) = -m*L(t) - q*L(t)*T(t)-ucte*L(t)^2+r2*C(t)*T(t) + pI*L(t)*I(t)/(gI+I(t)) + u1(t) - KL*M(t)*L(t), # tumor-specific effector cells, T-celss
     N'(t) = alpha1 - f*N(t)+ g*T(t)*N(t)/(h + T(t)) - p*N(t)*T(t) - KN*M(t)*N(t), # non-specific effector cells, NK cells
     C'(t) = alpha2 - beta*C(t) - KC*M(t)*C(t), #circulating lymphocytes
-    I'(t) = pt*T(t)*L(t)/(gt + T(t)) +  w*L(t)*I(t) - muI*I(t) + VI(t), # IL-2
-    M'(t) = - gamma*M(t) + VM(t), #chemotherapy drug
+    I'(t) = pt*T(t)*L(t)/(gt + T(t)) +  w*L(t)*I(t) - muI*I(t) + u2(t), # IL-2, VI = u2 aplicación directa, terapia de IL2
+    M'(t) = - gamma*M(t) + u1(t), #chemotherapy drug, terapia/aplicación de quimio, u1 = VM
     y1(t) = L(t) + N(t),
     Y4(t) = C(t),
     y2(t) = M(t),
