eba7a86eb19dee17dbd85d81c8e25596551510d4
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-12-28 13:07:35 +0100

    Update TumorPillis2007.jl

--- Models/Examples_Inmunology/SIAN/TumorPillis2007.jl
+++ Models/Examples_Inmunology/SIAN/TumorPillis2007.jl
@@ -96,7 +96,7 @@ ode = @ODEmodel(
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
 
-#
+#NO FUNCIONA
 using SIAN, Logging
 
 ode = @ODEmodel(
