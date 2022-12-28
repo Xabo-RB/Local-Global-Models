cb47963a8242517166d15b5c1bc5a5363d33be9f
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-12-28 19:24:02 +0100

    Update LeukaemiaLeon2021.jl

--- Models/Examples_Inmunology/SIAN/LeukaemiaLeon2021.jl
+++ Models/Examples_Inmunology/SIAN/LeukaemiaLeon2021.jl
@@ -12,11 +12,26 @@ ode = @ODEmodel(
     I'(t) = rhoi*(2*ai* (1/(1 + ks*(P(t)+I(t)))) -1 )*I(t) - I(t)/taoi + P(t)/taop - alpha*beta*I(t)*C(t), # number of cells of CD19+ B cell progenitors
     y1(t) = C(t),
     y2(t) = L(t),
-    y3(t) = P(t),
-    y4(t) =  B(t) + I(t) #células B totales 
+    y3(t) = P(t)+  B(t) + I(t) #células B totales 
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
 
 
+
+using SIAN, Logging
+
+ode = @ODEmodel(
+    C'(t) = rhoc*(L(t)+B(t))*C(t) + rhob*I(t)*C(t) - C(t)/taoc, #number of cells of CAR T cells
+    L'(t) = rhol*L(t) - alpha*L(t)*C(t), # number of cells of leukaemic cells
+    B'(t) = I(t)/taoi - alpha*B(t)*C(t) - B(t)/taob, # number of cells of mature healthy B cells
+    P'(t) = rhop*(2*ap* (1/(1 + ks*(P(t)+I(t)))) -1)*P(t) - P(t)/taop, # number of cells of CD19- haematopoietic stem cells (HSCs)
+    I'(t) = rhoi*(2*ai* (1/(1 + ks*(P(t)+I(t)))) -1 )*I(t) - I(t)/taoi + P(t)/taop - alpha*beta*I(t)*C(t), # number of cells of CD19+ B cell progenitors
+    y1(t) = C(t),
+    y2(t) = L(t),
+    y3(t) = P(t),
+    y4(t) =  B(t) + I(t) #células B totales 
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
 
