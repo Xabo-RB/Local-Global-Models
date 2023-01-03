361f51d1d1d665c199ec816ab0e3389d134b1966
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-01-03 14:03:48 +0100

    Update SEIR_3.jl

--- Models/Examples_COVID_Massonis/SIAN/SEIR_3.jl
+++ Models/Examples_COVID_Massonis/SIAN/SEIR_3.jl
@@ -72,3 +72,21 @@ ode = @ODEmodel(
     y4(t) = N(t)
 )
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+#
+using SIAN, Logging
+ode = @ODEmodel(
+    N'(t) = 0,
+    S'(t) = beta*I(t)*S(t)/N(t)-alpha*S(t),
+    E'(t) = beta*I(t)*S(t)/N(t)-gamma + E(t), #deshice la simetría al separar las variables
+    I'(t) = gamma + E(t)-delta*I(t),
+    R'(t) = delta*I(t)-alpha*Q(t)-k*Q(t),
+    Q'(t) = alpha*Q(t),
+    D'(t) = k*Q(t),
+    P'(t) = alpha*S(t),
+    y1(t) = Q(t),
+    y2(t) = D(t),
+    y3(t) = R(t),
+    y4(t) = N(t)
+)
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
