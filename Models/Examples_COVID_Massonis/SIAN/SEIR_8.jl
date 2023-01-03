cff71070d0fcd17bbca295fba57fa85f5cdabc50
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-01-03 16:58:48 +0100

    Update SEIR_8.jl

--- Models/Examples_COVID_Massonis/SIAN/SEIR_8.jl
+++ Models/Examples_COVID_Massonis/SIAN/SEIR_8.jl
@@ -18,3 +18,59 @@ ode = @ODEmodel(
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
 
+#PRUEBAS
+#no cambia nada
+using SIAN, Logging
+
+ode = @ODEmodel(
+    S'(t) = -beta*(I(t)+eta*A(t))*S(t)/N(t),
+    E'(t) = -beta*(I(t)+eta*A(t))*S(t)/N(t)-sigma*E(t), #AÑADÍ SIGNO
+    I'(t) = alpha*sigma*E(t)-phi*I(t)-gamma_i*I(t),
+    A'(t) = (1-alpha)*sigma*E(t)-gamma_0*A(t),
+    H'(t) = phi*I(t)-delta*H(t)-gamma_h*H(t),
+    R'(t) = gamma_i*I(t)+gamma_0*A(t)+gamma_h*H(t),
+    D'(t) = delta*H(t),
+    y1(t) = I(t),
+    y2(t) = H(t),
+    y3(t) = D(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+
+#no cambia nada
+using SIAN, Logging
+
+ode = @ODEmodel(
+    S'(t) = -beta*(I(t)+eta*A(t))*S(t)/N(t),
+    E'(t) = beta*(I(t)+eta*A(t))*S(t)/N(t)-sigma*E(t), 
+    I'(t) = alpha*sigma*E(t)-phi*I(t),#-gamma_i*I(t), ELIMINADO
+    A'(t) = (1-alpha)*sigma*E(t)-gamma_0*A(t),
+    H'(t) = phi*I(t)-delta*H(t)-gamma_h*H(t),
+    R'(t) = gamma_i*I(t)+gamma_0*A(t)+gamma_h*H(t),
+    D'(t) = delta*H(t),
+    y1(t) = I(t),
+    y2(t) = H(t),
+    y3(t) = D(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+
+#
+using SIAN, Logging
+
+ode = @ODEmodel(
+    S'(t) = -beta*(I(t)+eta*A(t))*S(t)/N(t),
+    E'(t) = beta*(I(t)+eta*A(t))*S(t)/N(t)-sigma*E(t), 
+    I'(t) = alpha*sigma*E(t)-phi*I(t)-gamma_i*I(t),
+    A'(t) = (1-alpha)*sigma*E(t)-gamma_0*A(t),
+    H'(t) = phi*I(t)-delta*H(t)-gamma_h*H(t),
+    R'(t) = gamma_i*I(t)+gamma_0*A(t)+gamma_h*H(t),
+    D'(t) = delta*H(t),
+    y1(t) = I(t),
+    y2(t) = H(t),
+    y3(t) = D(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
