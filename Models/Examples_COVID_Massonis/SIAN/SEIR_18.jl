11b681685385917e8609a7d555a18f33ba074c75
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-01-04 20:04:25 +0100

    Update SEIR_18.jl

--- Models/Examples_COVID_Massonis/SIAN/SEIR_18.jl
+++ Models/Examples_COVID_Massonis/SIAN/SEIR_18.jl
@@ -32,7 +32,7 @@ ode = @ODEmodel(
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
 
 
-#
+#exactamente sin cambios
 using SIAN, Logging
 ode = @ODEmodel(
     S'(t) = -beta*S(t)*(I(t)+theta*A(t))-pp*S(t)+lambda*Q(t),
@@ -47,3 +47,17 @@ ode = @ODEmodel(
 )
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
 
+#no cambia nada
+using SIAN, Logging
+ode = @ODEmodel(
+    S'(t) = -beta*S(t)*(I(t)+theta*A(t))-pp*S(t)+lambda*Q(t),
+    Q'(t) = pp*S(t)-lambda*Q(t),
+    E'(t) = beta*S(t)*(I(t)+theta*A(t))-sigma*E(t),
+    A'(t) = sigma*(1-rho)*E(t)-e_a*A(t)-gamma_a*A(t),
+    I'(t) = rho*E(t)-gamma_i*I(t)-d_i*I(t)-e_i*I(t), #elimino el sigma de aquí
+    D'(t) = e_a*A(t)+e_i*I(t)-d_d*D(t)-gamma_d*D(t),
+    R'(t) = gamma_a*A(t) + gamma_i*I(t) + gamma_d*D(t),
+    y1(t) = D(t),
+    y2(t) = Q(t)
+)
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
