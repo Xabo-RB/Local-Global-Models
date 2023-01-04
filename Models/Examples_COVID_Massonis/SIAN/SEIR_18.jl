1768a20299077a1a68bae5c6ff05f68b7c6284c6
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-01-04 12:45:20 +0100

    Update SEIR_18.jl

--- Models/Examples_COVID_Massonis/SIAN/SEIR_18.jl
+++ Models/Examples_COVID_Massonis/SIAN/SEIR_18.jl
@@ -32,13 +32,13 @@ ode = @ODEmodel(
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
 
 
-#Se eliminan los SLI
+#
 using SIAN, Logging
 ode = @ODEmodel(
     S'(t) = -beta*S(t)*(I(t)+theta*A(t))-pp*S(t)+lambda*Q(t),
     Q'(t) = pp*S(t)-lambda*Q(t),
     E'(t) = beta*S(t)*(I(t)+theta*A(t))-sigma*E(t),
-    A'(t) = (1-rho)*E(t)-e_a*A(t)-gamma_a*A(t),
+    A'(t) = (1-rho)*E(t)-e_a*A(t)-gamma_a*A(t), #elimino el sigma de aquí
     I'(t) = sigma*rho*E(t)-gamma_i*I(t)-d_i*I(t)-e_i*I(t),
     D'(t) = e_a*A(t)+e_i*I(t)-d_d*D(t)-gamma_d*D(t),
     R'(t) = gamma_a*A(t) + gamma_i*I(t) + gamma_d*D(t),
