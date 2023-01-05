ba7897eaea48854b5fda41481fd498260e95815e
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-01-05 18:05:37 +0100

    Update SIRSwithforcing.jl

--- Models/Examples_Benchmarking/SIAN julia/SIRSwithforcing.jl
+++ Models/Examples_Benchmarking/SIAN julia/SIRSwithforcing.jl
@@ -18,3 +18,34 @@ ode = @ODEmodel(
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
 
 # @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 0, nthrds = 1))
+
+
+#PRUEBAS
+#No cambia nada
+using Logging,SIAN
+ode = @ODEmodel(
+  s'(t) = mu - mu * s(t) - b0 * (1 + b1 * x1(t)) * i(t) * s(t) + g * r(t),
+  i'(t) = b0 * (1 + b1 * x1(t)) * i(t) * s(t) - (nu + mu) * i(t),
+  r'(t) = nu * i(t) - (mu + g) * r(t),
+  x1'(t) = -M * x2(t),
+  x2'(t) = -M * x1(t), #cambié este signo
+  y1(t) = i(t),
+  y2(t) = r(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+
+#No cambia nada
+using Logging,SIAN
+ode = @ODEmodel(
+  s'(t) = mu - mu * s(t) - b0 * (1 + b1 * x1(t)) * i(t) * s(t) + g * r(t),
+  i'(t) = b0 * (1 + b1 * x1(t)) * i(t) * s(t) - (nu + mu) * i(t),
+  r'(t) = nu * i(t) - (mu + g) * r(t),
+  x1'(t) = -M * x2(t),
+  x2'(t) = x1(t), #cambié este signo
+  y1(t) = i(t),
+  y2(t) = r(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
