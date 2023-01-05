ba634244a346cce621cc17d26a4fd14e59184c7f
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-01-05 13:03:50 +0100

    Update SEIR_38.jl

--- Models/Examples_COVID_Massonis/SIAN/SEIR_38.jl
+++ Models/Examples_COVID_Massonis/SIAN/SEIR_38.jl
@@ -64,7 +64,7 @@ ode = @ODEmodel(
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
 
-#
+#No cambia nada
 using SIAN, Logging
 ode = @ODEmodel(
     sigma'(t) = 0,
@@ -83,3 +83,44 @@ ode = @ODEmodel(
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+#No cambia nada
+using SIAN, Logging
+ode = @ODEmodel(
+    sigma'(t) = 0,
+    q'(t) = 0,
+    S'(t) = -(c*be+c+q(t)*(1-be))*S(t)*(I(t)+theta*E(t))+la*Sq(t),
+    E'(t) = c*be*(1-q(t))*S(t)*(I(t)+theta*E(t))-sigma(t)*E(t),
+    I'(t) = sigma(t)*E(t)-(delta_i+alpha+gamma_i)*I(t),
+    Sq'(t) = c*q(t)*(1-be)*S(t)*(I(t)+theta*E(t))-la*Sq(t),
+    Eq'(t) = c*be*q(t)*S(t)*(I(t)+theta*E(t))-delta_q*Eq(t),
+    H'(t) = delta_i*I(t)+delta_q*Eq(t)-(alpha+gamma_h)*H(t),
+    R'(t) = I(t)+gamma_h*H(t),#gamma i de ahí eliminada
+    y1(t) = I(t)*vi,
+    y2(t) = R(t)*vr,
+    y3(t) = sigma(t),
+    y4(t) = q(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+#
+using SIAN, Logging
+ode = @ODEmodel(
+    sigma'(t) = 0,
+    q'(t) = 0,
+    S'(t) = -(c*be+c+q(t)*(1-be))*S(t)*(I(t)+theta*E(t))+la*Sq(t),
+    E'(t) = c*be*(1-q(t))*S(t)*(I(t)+theta*E(t))-sigma(t)*E(t),
+    I'(t) = sigma(t)*E(t)-(delta_i+alpha)*I(t),#gamma i eliminado
+    Sq'(t) = c*q(t)*(1-be)*S(t)*(I(t)+theta*E(t))-la*Sq(t),
+    Eq'(t) = c*be*q(t)*S(t)*(I(t)+theta*E(t))-delta_q*Eq(t),
+    H'(t) = delta_i*I(t)+delta_q*Eq(t)-(alpha+gamma_h)*H(t),
+    R'(t) = gamma_i*I(t)+gamma_h*H(t),
+    y1(t) = I(t)*vi,
+    y2(t) = R(t)*vr,
+    y3(t) = sigma(t),
+    y4(t) = q(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
