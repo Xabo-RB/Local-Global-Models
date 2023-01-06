e952b5c4395c185c88d3649975688d2a72162a59
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-01-06 12:18:32 +0100

    Update SEIR.jl

--- Models/Examples_SIANjl/SEIR.jl
+++ Models/Examples_SIANjl/SEIR.jl
@@ -85,7 +85,7 @@ ode = @ODEmodel(
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
 
 
-#
+#TODO SGI
 using SIAN, Logging
 ode = @ODEmodel(
   S'(t) = -b * S(t) * In(t) / N(t),
