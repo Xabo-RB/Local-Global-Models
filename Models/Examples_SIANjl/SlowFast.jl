e5ba72ae563010786368178d3485426638074e66
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-01-06 17:43:22 +0100

    Update SlowFast.jl

--- Models/Examples_SIANjl/SlowFast.jl
+++ Models/Examples_SIANjl/SlowFast.jl
@@ -28,7 +28,7 @@ ode = @ODEmodel(
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
 
-#
+#todo SGI
 using SIAN, Logging
 ode = @ODEmodel(
   xA'(t) = -k1 * xA(t),
