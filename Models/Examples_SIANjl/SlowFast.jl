2f35bcd882aa675214ae3aad0232ddbfc970daff
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-01-06 12:21:58 +0100

    Update SlowFast.jl

--- Models/Examples_SIANjl/SlowFast.jl
+++ Models/Examples_SIANjl/SlowFast.jl
@@ -12,15 +12,10 @@ ode = @ODEmodel(
   y4(t) = eC(t)
 )
 
-res = identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 0, nthrds = 1)
-
-println(res)
-
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
 
 
-#SIN U O CONSTANTES CONOCIDAS
-
+#SIN U O CONSTANTES CONOCIDAS, todo igual
 using SIAN, Logging
 
 ode = @ODEmodel(
@@ -31,8 +26,68 @@ ode = @ODEmodel(
   y2(t) = xA(t) + xB(t) + xC(t)
 )
 
-res = identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 0, nthrds = 1)
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
 
-println(res)
+#
+using SIAN, Logging
+ode = @ODEmodel(
+  xA'(t) = -k1 * xA(t),
+  xB'(t) = xA(t) - k2 * xB(t), #elimino k1
+  xC'(t) = k2 * xB(t),
+  eA'(t) = 0,
+  eC'(t) = 0,
+  y1(t) = xC(t),
+  y2(t) = eA(t) * xA(t) + eB * xB(t) + eC(t) * xC(t),
+  y3(t) = eA(t),
+  y4(t) = eC(t)
+)
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+#
+using SIAN, Logging
+ode = @ODEmodel(
+  xA'(t) = -k1 * xA(t),
+  xB'(t) = k1 * xA(t) - xB(t), #elimino k2
+  xC'(t) = k2 * xB(t),
+  eA'(t) = 0,
+  eC'(t) = 0,
+  y1(t) = xC(t),
+  y2(t) = eA(t) * xA(t) + eB * xB(t) + eC(t) * xC(t),
+  y3(t) = eA(t),
+  y4(t) = eC(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+
+#
+using SIAN, Logging
+ode = @ODEmodel(
+  xA'(t) = -k1 * xA(t),
+  xB'(t) = k1 * xA(t) - k2*xB(t), #elimino k2
+  xC'(t) = xB(t), #elimino k2
+  eA'(t) = 0,
+  eC'(t) = 0,
+  y1(t) = xC(t),
+  y2(t) = eA(t) * xA(t) + eB * xB(t) + eC(t) * xC(t),
+  y3(t) = eA(t),
+  y4(t) = eC(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+#
+using SIAN, Logging
+ode = @ODEmodel(
+  xA'(t) = -xA(t), #elimino k1
+  xB'(t) = k1 * xA(t) - k2 * xB(t),
+  xC'(t) = k2 * xB(t),
+  eA'(t) = 0,
+  eC'(t) = 0,
+  y1(t) = xC(t),
+  y2(t) = eA(t) * xA(t) + eB * xB(t) + eC(t) * xC(t),
+  y3(t) = eA(t),
+  y4(t) = eC(t)
+)
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
