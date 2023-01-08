015170781e580c222188fc6622fd80820446069b
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-01-08 13:17:27 +0100

    Update LLW1987.jl

--- Models/Examples_Sedoglavic/StructuralIdentifiability/LLW1987.jl
+++ Models/Examples_Sedoglavic/StructuralIdentifiability/LLW1987.jl
@@ -47,7 +47,7 @@ ode = @ODEmodel(
 
 @time println(assess_identifiability(ode))
 
-#
+#p1 y p3 a SGI
 using SIAN, Logging
 ode = @ODEmodel(
     x1'(t) = - x1(t) + p2*u(t),#elimino p1
@@ -63,6 +63,36 @@ ode = @ODEmodel(
     x1'(t) = - p1*x1(t) + p2*u(t),
     x2'(t) = - p3*x2(t) + p4*u(t),
     x3'(t) = - (p1+p3)*x3(t) + (p4*x1(t)+p2*x2(t))*u(t),
+    y1(t) = x3(t)
+)
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+#p1 y p3 SGI
+using SIAN, Logging
+ode = @ODEmodel(
+    x1'(t) = - p1*x1(t) + p2*u(t),
+    x2'(t) = x2(t) + p4*u(t),#elimino p3
+    x3'(t) = - (p1+p3)*x3(t) + (p4*x1(t)+p2*x2(t))*u(t),
+    y1(t) = x3(t)
+)
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+#p1 y p3 SGI
+using SIAN, Logging
+ode = @ODEmodel(
+    x1'(t) = - p1*x1(t) + p2*u(t),
+    x2'(t) = -p3*x2(t) + p4*u(t),
+    x3'(t) = - (p1)*x3(t) + (p4*x1(t)+p2*x2(t))*u(t),#elimino p3
+    y1(t) = x3(t)
+)
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+#p1 y p3 SGI
+using SIAN, Logging
+ode = @ODEmodel(
+    x1'(t) = - p1*x1(t) + p2*u(t),
+    x2'(t) = -p3*x2(t) + p4*u(t),
+    x3'(t) = - (p3)*x3(t) + (p4*x1(t)+p2*x2(t))*u(t),#elimino p1
     y1(t) = x3(t)
 )
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
