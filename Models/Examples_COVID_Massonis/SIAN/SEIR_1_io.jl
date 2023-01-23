b21542eaf4d3ff898efa30a6bde4773955aa8925
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-01-23 17:50:50 +0100

    Update SEIR_1_io.jl

--- Models/Examples_COVID_Massonis/SIAN/SEIR_1_io.jl
+++ Models/Examples_COVID_Massonis/SIAN/SEIR_1_io.jl
@@ -1,7 +1,14 @@
 # Research about the optimal strategies for prevention and control 
 # of varicella outbreak in a school in a central city of China: based on an SEIR dynamic model
 
-using SIAN, Logging
+using Logging
+
+using StructuralIdentifiability
+
+logger = Logging.SimpleLogger(stdout, Logging.Info)
+global_logger(logger)
+
+
 ode = @ODEmodel(
     S'(t) = -beta*S(t)*I(t),
     E'(t) =  beta*S(t)*I(t)-v*E(t),
@@ -10,42 +17,7 @@ ode = @ODEmodel(
     Q'(t) = -gamma*Q(t)+psi*I(t),
     y1(t) = Q(t)
 )
-@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+@time println(assess_identifiability(ode))
 
+find_ioequations(ode, [var_change_policy=:default])
 
-#PRUEBAS
-#
-using SIAN, Logging
-ode = @ODEmodel(
-    S'(t) = beta*S(t)*I(t), #quité el signo
-    E'(t) = beta*S(t)*I(t)-v*E(t),
-    I'(t) = v*E(t)-psi*I(t)-(1-psi)*gamma*I(t),
-    R'(t) = gamma*Q(t)+(1-psi)*gamma*I(t),
-    Q'(t) = -gamma*Q(t)+psi*I(t),
-    y1(t) = Q(t)
-)
-@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
-
-#
-using SIAN, Logging
-ode = @ODEmodel(
-    S'(t) = beta*S(t)*I(t), #quité el signo
-    E'(t) = beta*S(t)*I(t)-v*E(t),
-    I'(t) = -v*E(t)-psi*I(t)-(1-psi)*gamma*I(t),#añadí el signo
-    R'(t) = gamma*Q(t)+(1-psi)*gamma*I(t),
-    Q'(t) = -gamma*Q(t)+psi*I(t),
-    y1(t) = Q(t)
-)
-@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
-
-#
-using SIAN, Logging
-ode = @ODEmodel(
-    S'(t) = -beta*S(t)*I(t), 
-    E'(t) = beta*S(t)*I(t)-v*E(t),
-    I'(t) = v + E(t)-psi*I(t)-(1-psi)*gamma*I(t), #deshice simetría
-    R'(t) = gamma*Q(t)+(1-psi)*gamma*I(t),
-    Q'(t) = -gamma*Q(t)+psi*I(t),
-    y1(t) = Q(t)
-)
-@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
