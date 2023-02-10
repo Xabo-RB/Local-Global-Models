691131e28b131e0601f84835170eaf9b00109070
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-02-10 10:54:25 +0100

    Update DAISY_ex3_io.jl

--- Models/Examples_SIANjl/DAISY_ex3_io.jl
+++ Models/Examples_SIANjl/DAISY_ex3_io.jl
@@ -20,8 +20,20 @@ ode = @ODEmodel(
   x2'(t) = p3 * x1(t) - p4 * x2(t) + x3(t),
   x3'(t) = p6 * x1(t) - p7 * x3(t),
   u0'(t) = 1,
-  y(t) = x1(t),
-  y2(t) = u0(t)
+  y(t) = x1(t)
+)
+
+
+println(find_ioequations(ode))
+
+
+using StructuralIdentifiability
+
+ode = @ODEmodel(
+  x1'(t) = -1 * p1 * x1(t) + x2(t) + u(t),
+  x2'(t) = p3 * x1(t) - p4 * x2(t) + x3(t),
+  x3'(t) = p6 * x1(t) - p7 * x3(t),
+  y(t) = x1(t)
 )
 
 
