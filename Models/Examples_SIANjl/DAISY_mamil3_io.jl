c7ac5900d103c5200262928cc6f2f71c4ad71fd3
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-02-13 11:42:16 +0100

    io

--- Models/Examples_SIANjl/DAISY_mamil3_io.jl
+++ Models/Examples_SIANjl/DAISY_mamil3_io.jl
@@ -9,3 +9,16 @@ ode = @ODEmodel(
 
 
 println(find_ioequations(ode))
+
+
+using StructuralIdentifiability
+
+ode = @ODEmodel(
+    x1'(t) = -(a21 + a31 + a01) * x1(t) + a12 * x2(t) + a13 * x3(t),
+    x2'(t) = a21 * x1(t) - a12 * x2(t),
+    x3'(t) = a31 * x1(t) - a13 * x3(t),
+    y(t) = x1(t)
+)
+
+
+println(find_ioequations(ode))
