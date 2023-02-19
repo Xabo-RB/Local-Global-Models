d45a93c1a9ad92db5a5513e178e7c76b7976e184
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-02-19 20:27:21 +0100

    Create SEIR_16_io.jl

---
+++ Models/Examples_COVID_Massonis/SIAN/SEIR_16_io.jl
@@ -0,0 +1,11 @@
+using StructuralIdentifiability
+
+ode = @ODEmodel(
+    S'(t) = -beta*I(t)*S(t),
+    E'(t) = beta*I(t)*S(t)-epsilon*E(t),
+    I'(t) = epsilon*E(t)-(rho+mu)*I(t),
+    R'(t) = rho*I(t)-d*R(t),
+    y1(t) = mu*I(t)
+)
+
+println(find_ioequations(ode))
