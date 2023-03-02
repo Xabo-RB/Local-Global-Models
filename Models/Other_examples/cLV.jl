16917512b460c3ea092857034961dbbc932699c6
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-03-02 12:53:44 +0100

    Update cLV.jl

--- Models/Other_examples/cLV.jl
+++ Models/Other_examples/cLV.jl
@@ -42,3 +42,17 @@ ode = @ODEmodel(
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+
+# 1 less output
+
+using SIAN, Logging
+ode = @ODEmodel(
+    pi1'(t) = pi1(t)*( (g1+A11*pi1(t)+A12*pi2(t)+A13*pi3(t)+B11*u1(t)) - pi1(t)*(g1+A11*pi1(t)+A12*pi2(t)+A13*pi3(t)+B11*u1(t)) + pi2(t)*(g2+A21*pi1(t)+A22*pi2(t)+A23*pi3(t)+B21*u1(t))),
+    pi2'(t) = pi2(t)*( (g2+A21*pi1(t)+A22*pi2(t)+A23*pi3(t)+B21*u1(t)) - pi1(t)*(g1+A11*pi1(t)+A12*pi2(t)+A13*pi3(t)+B11*u1(t)) + pi2(t)*(g2+A21*pi1(t)+A22*pi2(t)+A23*pi3(t)+B21*u1(t))),
+    pi3'(t) = pi3(t)*( (g3+A31*pi1(t)+A32*pi2(t)+A33*pi3(t)+B31*u1(t)) - pi1(t)*(g1+A11*pi1(t)+A12*pi2(t)+A13*pi3(t)+B11*u1(t)) + pi2(t)*(g2+A21*pi1(t)+A22*pi2(t)+A23*pi3(t)+B21*u1(t))),
+    y1(t) = pi1(t),
+    y2(t) = pi2(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
