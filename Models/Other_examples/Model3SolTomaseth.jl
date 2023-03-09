7eaa6ed315e027b76f4768a5f5bec47415f9f468
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-03-09 13:00:07 +0100

    Update Model3SolTomaseth.jl

--- Models/Other_examples/Model3SolTomaseth.jl
+++ Models/Other_examples/Model3SolTomaseth.jl
@@ -6,7 +6,7 @@ using StructuralIdentifiability
 ode = @ODEmodel(
     x1'(t) = -(k01 + k21 + k31)*x1(t) + k12*x2(t) + u(t),
     x2'(t) = k21*x1(t) - (k12 + k32)*x2(t) + k23*x3(t),
-    I'(t) = k31*x1(t) + k32*x2(t) - (k23+k03)*x3(t),
+    x3'(t) = k31*x1(t) + k32*x2(t) - (k23+k03)*x3(t),
     y1(t) = x2(t),
     y2(t) = x3(t)
 )
