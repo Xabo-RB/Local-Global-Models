1b630f62c9338b6836b449d0d5ac9baabd3d7442
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-03-21 10:14:57 +0100

    Update 2DOF.jl

--- Models/Examples_StrikeGoldd/SIAN SI jl/2DOF.jl
+++ Models/Examples_StrikeGoldd/SIAN SI jl/2DOF.jl
@@ -10,3 +10,19 @@ ode = @ODEmodel(
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
 
+
+#u2 unkwonw input
+using SIAN, Logging
+ode = @ODEmodel(
+    x1'(t) = dx1(t),
+    x2'(t) = dx2(t),
+    dx1'(t) = 1/m1*( -(k1+dk1*x1(t))*x1(t)+k2*(x2(t)-x1(t))-c1*dx1(t)+c2*(dx2(t)-dx1(t))+u1(t) ),
+    dx2'(t) = 1/m2*( k2*(x1(t)-x2(t))+c2*(dx1(t)-dx2(t))+u2(t)),
+    u2'(t) = 1,
+    y1(t) = x1(t),
+    y2(t) = 1/m2*( k2*(x1(t)-x2(t))+c2*(dx1(t)-dx2(t))+u2(t))
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+
