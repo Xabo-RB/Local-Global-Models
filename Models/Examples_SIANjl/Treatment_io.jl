c6dec32f9accbf584addce191386e7ff696468d8
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-03-28 18:06:47 +0200

    Create Treatment_io.jl

---
+++ Models/Examples_SIANjl/Treatment_io.jl
@@ -0,0 +1,12 @@
+using StructuralIdentifiability
+
+ode = @ODEmodel(
+  S'(t) = -b * S(t) * In(t) / N(t) - d * b * S(t) * Tr(t) / N(t),
+  In'(t) = b * S(t) * In(t) / N(t) + d * b * S(t) * Tr(t) / N(t) - (a + g) * In(t),
+  Tr'(t) = g * In(t) - nu * Tr(t),
+  N'(t) = 0,
+  y1(t) = Tr(t),
+  y2(t) = N(t)
+)
+
+println(find_ioequations(ode))
