074cf20ade879da6ae9c593d50932cb8e775f60c
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-03-29 12:04:52 +0200

    seir

--- Models/Other_examples/SEIR.jl
+++ Models/Other_examples/SEIR.jl
@@ -7,6 +7,7 @@ ode = @ODEmodel(
     E'(t) =  beta*I(t)*(S(t)/N(t)) - alpha*E(t),
     I'(t) = alpha*E(t) - lambda*I(t),
     R'(t) = lambda*I(t),
-    y1(t) = I(t)
+    y1(t) = I(t),
+    y2(t) = N(t)
 )
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
