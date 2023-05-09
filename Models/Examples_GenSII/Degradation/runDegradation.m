815c66461dd8a74c062065074783c3a17a359703
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-05-09 12:33:33 +0200

    n

--- Models/Examples_GenSII/Degradation/runDegradation.m
+++
@@ -1,7 +0,0 @@
-% runDegradation runs the structural identifiability analysis for a 
-% simple model for protein synthesis and enzymatic degradation.
-
-% Confirm execution
-genssiAskForConfirmation(1);
-
-genssiMain('Degradation',10);
