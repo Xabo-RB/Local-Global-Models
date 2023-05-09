3b1c378f1be59e7f8457560639f873410b83da39
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-05-09 12:30:16 +0200

    n

--- Models/Examples_Inmunology/GenSII/TCRtrigJoao/runTCRtrigJoao_D.m
+++
@@ -1,6 +0,0 @@
-%genssiAskForConfirmation(1);
-
-syms s ki hh k L
-%genssiMain('TCRtrigJoao_E',7,[phi; s; ki; hh; k; L]);
-
-genssiMain('TCRtrigJoao_D',11);
--- Models/Examples_Inmunology/GenSII/TCRtrigJoao/runTCRtrigJoao_E.m
+++
@@ -1,8 +0,0 @@
-
-%genssiAskForConfirmation(1);
-
-%
-syms lambda phi s ki hh k L
-%genssiMain('TCRtrigJoao_E',7,[phi; s; ki; hh; k; L]);
-
-genssiMain('TCRtrigJoao_E',11,[hh; k; L]);
