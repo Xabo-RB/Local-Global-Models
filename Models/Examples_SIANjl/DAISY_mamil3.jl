c7f18fe68c1722474aa666d6903224a5341f9561
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-05-10 10:23:59 +0200

    Update DAISY_mamil3.jl

--- Models/Examples_SIANjl/DAISY_mamil3.jl
+++ Models/Examples_SIANjl/DAISY_mamil3.jl
@@ -26,3 +26,37 @@ ode = @ODEmodel(
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+
+#cambiando colocación da variable de entrada TODO SGI
+using SIAN, Logging
+
+ode = @ODEmodel(
+  x1'(t) = -(a21 + a31 + a01) * x1(t) + a12 * x2(t) + a13 * x3(t),
+  x2'(t) = a21 * x1(t) - a12 * x2(t) + u(t),
+  x3'(t) = a31 * x1(t) - a13 * x3(t),
+  y(t) = x1(t)
+)
+
+res = identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 0, nthrds = 1)
+
+println(res)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+
+#cambiando colocación da variable de entrada TODO SGI
+using SIAN, Logging
+
+ode = @ODEmodel(
+  x1'(t) = -(a21 + a31 + a01) * x1(t) + a12 * x2(t) + a13 * x3(t),
+  x2'(t) = a21 * x1(t) - a12 * x2(t),
+  x3'(t) = a31 * x1(t) - a13 * x3(t) +  u(t),
+  y(t) = x1(t)
+)
+
+res = identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 0, nthrds = 1)
+
+println(res)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
