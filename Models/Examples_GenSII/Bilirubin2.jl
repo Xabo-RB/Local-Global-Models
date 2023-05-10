dd1f6bf11d83e72a33a4d9e73a82ffea235a54b5
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-05-10 10:27:37 +0200

    Update Bilirubin2.jl

--- Models/Examples_GenSII/Bilirubin2.jl
+++ Models/Examples_GenSII/Bilirubin2.jl
@@ -129,3 +129,42 @@ ode = @ODEmodel(
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+#cambiando u de lugar
+using SIAN, Logging
+
+ode = @ODEmodel(
+  x1'(t) = -(k21+k31+k41+k01)*x1(t)+k12*x2(t)+k13*x3(t)+k14*x4(t),
+  x2'(t) = k21*x1(t)-k12*x2(t),
+  x3'(t) = k31*x1(t)-k13*x3(t),
+  x4'(t) = k41*x1(t)-k14*x4(t) +u(t),
+  y1(t) = x1(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+#cambiando u de lugar
+using SIAN, Logging
+
+ode = @ODEmodel(
+  x1'(t) = -(k21+k31+k41+k01)*x1(t)+k12*x2(t)+k13*x3(t)+k14*x4(t),
+  x2'(t) = k21*x1(t)-k12*x2(t) + u(t),
+  x3'(t) = k31*x1(t)-k13*x3(t),
+  x4'(t) = k41*x1(t)-k14*x4(t),
+  y1(t) = x1(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+
+#cambiando u de lugar
+using SIAN, Logging
+
+ode = @ODEmodel(
+  x1'(t) = -(k21+k31+k41+k01)*x1(t)+k12*x2(t)+k13*x3(t)+k14*x4(t),
+  x2'(t) = k21*x1(t)-k12*x2(t),
+  x3'(t) = k31*x1(t)-k13*x3(t) + u(t),
+  x4'(t) = k41*x1(t)-k14*x4(t),
+  y1(t) = x1(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
