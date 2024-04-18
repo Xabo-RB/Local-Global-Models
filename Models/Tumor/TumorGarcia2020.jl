f6cfb0052a72cc955a36296dd0b1aec284f4894d
Xabier Rey Barreiro <xabo.rey@gmail.com>
2024-04-18 15:57:01 +0200

    N

--- Models/Tumor/TumorGarcia2020.jl
+++ Models/Tumor/TumorGarcia2020.jl
@@ -10,5 +10,5 @@ ode = @ODEmodel(
     y1(t) = E(t)
 )
 
-@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10))
 
--- Models/Tumor/TumorHu2019.jl
+++ Models/Tumor/TumorHu2019.jl
@@ -61,5 +61,5 @@ ode = @ODEmodel(
     y4(t) = v(t)
 )
 
-@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10))
 
