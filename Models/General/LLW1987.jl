8438903d596db5be433bb67a1b485e8681e2c7df
Xabier Rey Barreiro <xabo.rey@gmail.com>
2025-07-17 09:46:16 +0200

    a

--- Models/General/LLW1987.jl
+++ Models/General/LLW1987.jl
@@ -9,6 +9,17 @@ ode = @ODEmodel(
 
 @time println(assess_identifiability(ode))
 
+using SIAN, Logging
+
+ode = @ODEmodel(
+    x1'(t) = - p1*x1(t) + p2*u(t),
+    x2'(t) = - p3*x2(t) + p4*u(t),
+    x3'(t) = - (p1+p3)*x3(t) + (p4*x1(t)+p2*x2(t))*u(t),
+    y1(t) = x3(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10))
+
 
 #SIN u(t)
 using StructuralIdentifiability
